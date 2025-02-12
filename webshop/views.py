6697524f698527c4f713ea9c9dd9f6df7d6f7c0d
Suphan <148407664+SuphanChainok@users.noreply.github.com>
2025-02-12 20:32:24 +0700

    first commit

---
+++ webshop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def indax(request) :
+    return render(request, 'index.html')
+
+def login(request) :
+    return render(request, 'login.html')
+
+def classic(request) :
+    return render(request, 'classic.html')
+
+def quiter(request) :
+    return render(request, 'quiter.html')
+
