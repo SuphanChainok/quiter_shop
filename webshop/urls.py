ad88cf4d079ab3aa48c9b6a1f340d6ded8a0ce81
Suphan <148407664+SuphanChainok@users.noreply.github.com>
2025-02-15 14:46:42 +0700

    order

--- webshop/urls.py
+++ webshop/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name= 'login'),
-    path("index/", views.indax, name= 'index'),
-    path("classic/", views.classic, name= 'classic'),
-    path("quiter/", views.quiter, name= 'quiter'),
+    path('', views.login, name='login'),
+    path("index/", views.indax, name='index'),
+    path("classic/", views.classic, name='classic'),
+    path("quiter/", views.quiter, name='quiter'),
     path("basket/", views.basket, name="basket"),
-    
-]
+    path("information/", views.information, name="information"),
 
+
+]
--- webshop/views.py
+++ webshop/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-def indax(request) :
+
+
+def indax(request):
     return render(request, 'index.html')
 
-def login(request) :
+
+def login(request):
     return render(request, 'login.html')
 
-def classic(request) :
+
+def classic(request):
     return render(request, 'classic.html')
 
-def quiter(request) :
+
+def quiter(request):
     return render(request, 'quiter.html')
 
-def basket(request) :
+
+def basket(request):
     return render(request, 'basket.html')
 
+
+def information(request):
+    return render(request, 'information.html')
